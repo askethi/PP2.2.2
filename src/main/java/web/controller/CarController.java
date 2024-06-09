cc688d4f0e0e6565fc17c447a3ae7d322f844f5a
askethi <wstrugalski@gmail.com>
2024-06-09 12:25:15 +0300

    commit

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    CarService cs = new CarService();
+
+    @GetMapping("/cars")
+    public String index(@RequestParam(defaultValue="5") int count, ModelMap model) {
+    //    model.addAttribute("message", "Hello Thymeleaf");
+        model.addAttribute("cars", cs.takeNCars(count));
+        return "cars";
+    }
+}
